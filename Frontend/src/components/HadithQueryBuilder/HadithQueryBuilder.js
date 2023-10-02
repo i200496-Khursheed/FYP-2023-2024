c9c3ef3d0d9e63aa8283b0b752731f164b9d29e2
i200496-Khursheed <i200496@nu.edu.pk>
2023-10-02 11:45:34 +0500

    9th Commit | continuing HQB

--- Frontend/src/components/HadithQueryBuilder/HadithQueryBuilder.js
+++ Frontend/src/components/HadithQueryBuilder/HadithQueryBuilder.js
@@ -1,9 +1,29 @@
+// HadithQueryBuilder.js
+
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Select from 'react-select';
 import './HadithQueryBuilder.css';
 
+const themeOptions = [
+  { value: 'theme1', label: 'Theme 1' },
+  { value: 'theme2', label: 'Theme 2' },
+  { value: 'theme3', label: 'Theme 3' },
+];
+
+const narratorTitleOptions = [
+  { value: 'title1', label: 'Title 1' },
+  { value: 'title2', label: 'Title 2' },
+  { value: 'title3', label: 'Title 3' },
+];
+
+const narratorNameOptions = [
+  { value: 'name1', label: 'Name 1' },
+  { value: 'name2', label: 'Name 2' },
+  { value: 'name3', label: 'Name 3' },
+];
+
 function HadithQueryBuilder() {
-  const [selectedOption, setSelectedOption] = useState('hadith'); // Set default to 'hadith'
+  const [selectedOption, setSelectedOption] = useState('hadith');
 
   const handleRadioChange = (option) => {
     setSelectedOption(option);
@@ -11,17 +31,17 @@ function HadithQueryBuilder() {
 
   return (
     <div className="hadith-query-builder">
-      <Link to="/" className="back-button">
+      <div className="back-button" onClick={() => window.history.back()}>
         <img src={require('../../assets/back_button.png')} alt="Back Button" />
-      </Link>
+      </div>
       <div className="radio-buttons">
         <label className={`radio-button ${selectedOption === 'hadith' ? 'selected' : ''}`}>
           <input
             type="radio"
             name="queryType"
             value="hadith"
-            checked={selectedOption === 'hadith'} // Set checked attribute
             onChange={() => handleRadioChange('hadith')}
+            checked={selectedOption === 'hadith'}
           />
           <span>Hadith</span>
         </label>
@@ -30,8 +50,8 @@ function HadithQueryBuilder() {
             type="radio"
             name="queryType"
             value="verse"
-            checked={selectedOption === 'verse'} // Set checked attribute
             onChange={() => handleRadioChange('verse')}
+            checked={selectedOption === 'verse'}
           />
           <span>Verse</span>
         </label>
@@ -40,11 +60,38 @@ function HadithQueryBuilder() {
             type="radio"
             name="queryType"
             value="commentary"
-            checked={selectedOption === 'commentary'} // Set checked attribute
             onChange={() => handleRadioChange('commentary')}
+            checked={selectedOption === 'commentary'}
           />
           <span>Commentary</span>
         </label>
+      </div>
+      <div className="query-box">
+        <div className="search-text">Search for Hadith with:</div>
+        <div className="dropdown">
+          <label htmlFor="theme">Theme</label>
+          <Select
+            options={themeOptions}
+            isSearchable={true}
+            onChange={(selectedOption) => console.log(selectedOption)}
+          />
+        </div>
+        <div className="dropdown">
+          <label htmlFor="narratorTitle">Narrator Title</label>
+          <Select
+            options={narratorTitleOptions}
+            isSearchable={true}
+            onChange={(selectedOption) => console.log(selectedOption)}
+          />
+        </div>
+        <div className="dropdown">
+          <label htmlFor="narratorName">Narrator Name</label>
+          <Select
+            options={narratorNameOptions}
+            isSearchable={true}
+            onChange={(selectedOption) => console.log(selectedOption)}
+          />
+        </div>
       </div>
     </div>
   );
