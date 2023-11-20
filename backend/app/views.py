f77cdbcb0aba05e139d8e16cd07b0998b37fdc40
SameerLiaquat <i200998@nu.edu.pk>
2023-11-20 15:25:03 +0500

    Generic API

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,7 @@
 #views.py
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from .models import *
 from .serializer import *
@@ -13,37 +15,65 @@ from .queryengine import Sparql_Endpoint, constructHadithSparQLQueryString
 class ReactView(APIView):
     print('sadsada')
     
-    def get(self, request):
-        output = [{"theme": output.theme,
-                   "narratorname": output.narratorname,
-                   "narratortitle": output.narratortitle}
-                  for output in React.objects.all()]
-        return Response(output)
+    # def get(self, request):
+    #     output = [{"theme": output.theme,
+    #                "narratorname": output.narratorname,
+    #                "narratortitle": output.narratortitle}
+    #               for output in React.objects.all()]
+    #     return Response(output)
     
-    def post(self, request):
-        print(request.data)
-        serializer = ReactSerializer(data=request.data)
-        print('serererer',serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Data saved successfully"})
-        else:
-            return Response(serializer.errors, status=400)
+    # def post(self, request):
+    #     print(request.data)
+    #     serializer = ReactSerializer(data=request.data)
+    #     print('serererer',serializer)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({"message": "Data saved successfully"})
+    #     else:
+    #         return Response(serializer.errors, status=400)
         
+# @csrf_exempt
+# def query_hadith(request):
+#     print("hello",request)
+#     print('popo',request)
+#     if request.method == 'GET':
+#         theme = request.GET.get('theme', '')  
+#         hadith_number = request.GET.get('hadith_number', None)  # Corrected to use `None` instead of `null`
+
+#         # Generate the SPARQL query based on the provided parameters
+#         if hadith_number is None:  # Corrected condition to check for `None`
+#             query = constructHadithSparQLQueryString(theme=theme)
+#         else:
+#             query = constructHadithSparQLQueryString(theme=theme, hadith_number=hadith_number)
+
+#         # Execute the SPARQL query and get the result
+#         prefix = "http://www.tafsirtabari.com/ontology"
+#         get_query = urllib.parse.quote(query)
+#         result = Sparql_Endpoint(get_query, prefix)
+#         print(result)
+
+#         return JsonResponse({'result': result})
+#     else:
+#         return JsonResponse({'error': 'Only GET requests are allowed for this endpoint'})
+
+
 @csrf_exempt
 def query_hadith(request):
-    print('popo',request)
-    if request.method == 'GET':
-        theme = request.GET.get('theme', '')  
-        hadith_number = request.GET.get('hadith_number', None)  # Corrected to use `None` instead of `null`
+    print('backend/POST')
+    if request.method == 'POST':  # Change to POST
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON in the request body'}, status=400)
 
-        # Generate the SPARQL query based on the provided parameters
-        if hadith_number is None:  # Corrected condition to check for `None`
+        theme = data.get('theme', '')
+        hadith_number = data.get('hadith_number', None)
+
+        if hadith_number is None:
             query = constructHadithSparQLQueryString(theme=theme)
         else:
             query = constructHadithSparQLQueryString(theme=theme, hadith_number=hadith_number)
 
-        # Execute the SPARQL query and get the result
         prefix = "http://www.tafsirtabari.com/ontology"
         get_query = urllib.parse.quote(query)
         result = Sparql_Endpoint(get_query, prefix)
@@ -51,5 +81,4 @@ def query_hadith(request):
 
         return JsonResponse({'result': result})
     else:
-        return JsonResponse({'error': 'Only GET requests are allowed for this endpoint'})
-
+        return JsonResponse({'error': 'Only POST requests are allowed for this endpoint'})
