87ee5bfbabf42c43b8d43d4f417d0d55293c61b6
i200496-Khursheed <95427284+i200496-Khursheed@users.noreply.github.com>
2024-04-30 19:53:49 +0500

    Job Fair
    Job Fair Version

---
+++ Backend/project/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import ReactView, chain_narrators, query_hadith, query_verse, query_commentary, query_federated, competency_question1, query_federated2
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="anything"),
+    path('api/query_hadith/', query_hadith, name='query_hadith'),
+    path('api/query_verse/', query_verse, name='query_verse'),
+    path('api/query_commentary/', query_commentary, name='query_commentary'),
+    path('api/query_federated/', query_federated, name='query_federated'),
+    path('api/competency_question1/', competency_question1, name='competency_question1'),
+    path('api/chain_narrators/', chain_narrators, name='chain_narrators'),
+
+    path('api/query_federated2/', query_federated2, name='query_federated2'),
+
+]
